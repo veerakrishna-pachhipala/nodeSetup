44641d2dd41d12e6da6343c9be4dadd9c53672a7
VeeraKrishna <krishna.automate@gmail.com>
2019-05-08 14:58:31 +0530

    config changed

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,6 +1,6 @@
-const mongopassword = "hVza3oSbG2Febbx8";
+const mongopassword = "";
 export default {
     name: 'dev',
-    secret: '$123abASSsdfasdf213123*dsf123DEVadfakdfj',
-    mongooseString: `mongodb+srv://root:${mongopassword}@cluster0-gaxbt.mongodb.net/test?retryWrites=true`
+    secret: 'your secret',
+    mongooseString: `your connection string`
 }
