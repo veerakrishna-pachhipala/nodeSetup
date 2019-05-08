5af4fb14906fe6a1480d482e80187ad0183fa8eb
VeeraKrishna <krishna.automate@gmail.com>
2019-05-08 14:53:55 +0530

    starter setup templae

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,6 +1,3 @@
-const mongopassword = "hVza3oSbG2Febbx8";
 export default {
-    name: 'dev',
-    secret: '$123abASSsdfasdf213123*dsf123DEVadfakdfj',
-    mongooseString: `mongodb+srv://root:${mongopassword}@cluster0-gaxbt.mongodb.net/test?retryWrites=true`
+    name: 'dev'
 }
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,3 @@
-const mongopassword = "hVza3oSbG2Febbx8";
 export default {
-    name: 'prod',
-    secret: '$123abASSsdfasdf213123*dsf123DEVadfakdfj',
-    mongooseString: `mongodb://root:${encodeURIComponent(mongopassword)}@cluster0-gaxbt.mongodb.net/test?retryWrites=true`
+    name: 'prod'
 }
